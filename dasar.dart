268be9c8ad954c73b35fd3762a3fbba1cce4bb19
Machfud Hidayat <machfudhidayat26@gmail.com>
2023-03-17 21:42:49 +0700

    menambah file dasar.dart & mengubah file luas_lingkaran.dart ke exercise3.dart

---
+++ dasar.dart
@@ -0,0 +1,42 @@
+void main() {
+  //variable
+  /*
+  var score = 90;
+  var nilai;
+  print(nilai);
+  print(score);
+  print('score');
+  */
+
+  //konstanta
+  /*
+  Untuk data yg tidak berubah/berubah disarankan pakai final
+  agar mempercepat proses running
+  
+  final phi = 3.14;
+  print(phi);
+  */
+
+  //tipe data
+  /*
+  var phi;
+  double phi1 = 0;
+  final phi2 = 0;
+  final double phi3 = 0;
+  String status = 'online\noke';
+  String status1 = 'online\rnim';
+
+  print(phi3.runtimeType);
+  print(status1);
+  */
+
+  //operator
+  print(5 % 2);
+  print(5 % 3);
+  print(4 % 3);
+  var x = 2, y = 90;
+  var hasil = y / x;
+  print(hasil);
+
+  //assignment
+}
---
+++ exercise3.dart
@@ -0,0 +1,7 @@
+main() {
+  final phi = 3.14;
+  final jari = 7;
+  var luasLingkaran = phi * jari * jari;
+
+  print(luasLingkaran);
+}
