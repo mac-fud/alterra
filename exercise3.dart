67ba85c49ac606ea29a33a32c59cc10da411bc7d
Machfud Hidayat <machfudhidayat26@gmail.com>
2023-03-18 06:45:57 +0700

    update exercise3.dart

--- exercise3.dart
+++ exercise3.dart
@@ -1,7 +1,15 @@
 main() {
+  //No 1
   final phi = 3.14;
   final jari = 7;
   var luasLingkaran = phi * jari * jari;
 
-  print(luasLingkaran);
+  print(luasLingkaran.roundToDouble());
+
+  //No 2
+  String firstName = 'Machfud';
+  String midName = 'Si';
+  String lastName = 'Fulan';
+
+  print('hello my name $firstName $midName $lastName');
 }
